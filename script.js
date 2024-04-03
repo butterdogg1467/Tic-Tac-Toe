5de318ddc571b316dfe0decd7195fd3e351cc4fc
butterdogg1467 <momink2600@gmail.com>
2024-04-03 19:52:20 -0400

    Added gameboard spaces, function for checking player input
    Added playerSelections array to keep track of player input
    Added gameboard space to check if there is no space remaining, if true it will be a tie
    Added if statements to playerInput to check for length of player input, and deduct number from gameboardSpace
    Added if statement to check when playerSelections is longer than 3, if true it joins the array and checks if it matches any pattern.

--- script.js
+++ script.js
@@ -21,15 +21,37 @@ function createPlayer(name) {
     let player = {
         name: name,
         points: 0,
-        wins: 0
+        wins: 0,
+        ties: 0,
     }
 
+    let gameboardSpace = 9
+
+    let playerSelections = []
+
     function playerInput(input){
-        if (input === 111){
-            addPoints()
+
+        if (typeof input === 'number'){
+            let inputStr = input.toString()
+            if (inputStr.length === 1) {
+            gameboardSpace = gameboardSpace - 1
+            playerSelections.push(input)
+            } else if (inputStr.length === 2) {
+                gameboardSpace = gameboardSpace - 2
+                playerSelections.push(input)
+            } else if (inputStr.length === 3) {
+                gameboardSpace = gameboardSpace - 3
+                playerSelections.push(input)
+            } 
         }
-        alert(input)
+        if (playerSelections.length >= 3) {
+            let playerSelectionsJoined = playerSelections.join('')
+            if (playerSelectionsJoined === '012' || playerSelectionsJoined === '000' || playerSelectionsJoined === '111' || playerSelectionsJoined === '222' || playerSelectionsJoined === '012') {
+                alert('nigga')
+            }
+        }
     }
+
 
     function addPoints() {
         player.points++
@@ -37,13 +59,17 @@ function createPlayer(name) {
             player.wins++
             player.points = 0
         }
+        if (gameboardSpace === 0 && player.wins === 0) {
+            alert ('tie!')
+        }
     }
 
     return {
         addPoints,  
         playerInput,
         name,
-        player
+        player,
+        playerSelections
     }
 }
 
