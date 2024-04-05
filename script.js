81678bd90d1ed5b031f2ca61d558a466fb4911b2
butterdogg1467 <momink2600@gmail.com>
2024-04-04 23:02:44 -0400

    Added player stat displays and form to submit username
    Added divs to display player1 and player2 usernames, points, and wins
    Added ids to griditems for game functionality
    Added inputs and buttons for players to input usernames
    Added function that will check if player1stats are filled, if true, player stats will be put in player2stats
    Added event listener to form buttons that create player with the name given in the text input
    Used document.queryselector to select all grid elements
    Added grid item clicked variables to keep track of whether a grid has already been picked by a player

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 document.addEventListener('DOMContentLoaded', function () {
 
+
 (function gameboard() {
     let rows = 3;
     let columns = 3;
@@ -14,8 +15,53 @@ document.addEventListener('DOMContentLoaded', function () {
 
     console.log(board)
 
-
 } ())
+let gridItem1 = document.querySelector('#griditem1')
+    let gridItem2 = document.querySelector('#griditem2')
+    let gridItem3 = document.querySelector('#griditem3')
+    let gridItem4 = document.querySelector('#griditem4')
+    let gridItem5 = document.querySelector('#griditem5')
+    let gridItem6 = document.querySelector('#griditem6')
+    let gridItem7 = document.querySelector('#griditem7')
+    let gridItem8 = document.querySelector('#griditem8')
+    let gridItem9 = document.querySelector('#griditem9')
+    
+    let gridItem1Clicked = false;
+    let gridItem2Clicked = false;
+    let gridItem3Clicked = false;
+    let gridItem4Clicked = false;
+    let gridItem5Clicked = false;
+    let gridItem6Clicked = false;
+    let gridItem7Clicked = false;
+    let gridItem8Clicked = false;
+    let gridItem9Clicked = false;
+    
+    gridItem1.addEventListener('click', function() {
+        gridItem1Clicked = true
+        alert(gridItem1Clicked)
+    })
+
+    let username1 = document.querySelector('#username1')
+    let username2 = document.querySelector('#username2')
+    let usernamebutton1 = document.querySelector('#username1button')
+    let usernamebutton2 = document.querySelector('#username2button')
+
+    usernamebutton1.addEventListener('click', function() {
+        createPlayer(username1.value)
+    })
+
+    usernamebutton2.addEventListener('click', function() {
+        createPlayer(username2.value)
+    })
+
+    let player1StatName = document.querySelector('.player1name')
+    let player1StatPoints = document.querySelector('.player1points')
+    let player1StatWins = document.querySelector('.player1wins')
+
+    let player2StatName = document.querySelector('.player2name')
+    let player2StatPoints = document.querySelector('.player2points')
+    let player2StatWins = document.querySelector('.player2wins')
+
 
 function createPlayer(name) {
     let player = {
@@ -23,6 +69,21 @@ function createPlayer(name) {
         points: 0,
         wins: 0,
         ties: 0,
+    }
+
+    if (player1StatName.textContent === '' && player1StatPoints.textContent === '' && player1StatWins.textContent === '') {
+        player1StatName.textContent = 'Name: ' + player.name
+        player1StatPoints.textContent = 'Points: ' + player.points
+        player1StatWins.textContent = 'Wins: ' + player.wins
+    } else {
+        player2StatName.textContent = 'Name: ' + player.name
+        player2StatPoints.textContent = 'Points: ' + player.points
+        player2StatWins.textContent = 'Wins: ' + player.wins
+    }
+    
+    if (gridItem1Clicked === true) {
+        playerSelections.push('1')
+        alert(playerSelections)
     }
 
     let gameboardSpace = 9
@@ -63,16 +124,17 @@ function createPlayer(name) {
             player.points = 0
         }
     }
+    
 
     return {
         addPoints,  
         playerInput,
         name,
         player,
-        playerSelections
+        playerSelections,
     }
-}
 
+}
 
 
 
